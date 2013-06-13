6dcaeb363ad82395dfde8a2dd0692c4d3022c1ad
chrystopher.chabert@gmail.com <chrystopher.chabert@gmail.com@280fe1f3-3fe8-b438-d6e9-2b0f9534f95b>
2013-06-13 00:37:37 +0000

    Ajout de commentaires additionnels.

--- trunk/labo2/src/application/MainPartTwo.java
+++ trunk/labo2/src/application/MainPartTwo.java
@@ -3,6 +3,11 @@
 	Session :         Été 2010
 	Groupe :          01
 	Projet :          Laboratoire #2
+	Étudiants : Artom Lifshitz 
+				Chrystophe Chabert
+	Code(s) perm. : LIFA29108505
+					CHAC12098902
+	Date dernière modif: 2013-06-11
 	Date création :   2010-05-21
 ******************************************************
 Exécutable de Application Deux. À l'exécution, l'objet
@@ -11,6 +16,8 @@ un lien avec le bus d'événements.
 
 Délai de traitement des événements : 2 secondes.
 Événement synchronisé : affiche le mot "Avez".
+
+Classe modifiée pour envoyer le ID au serveur.
 ******************************************************/ 
 package application;
 
@@ -34,6 +41,7 @@ public class MainPartTwo {
 		List<Class> listenedEvents = new ArrayList<Class>();
 		listenedEvents.add(IPartTwoEvent.class);
 		listenedEvents.add(IEventSynchronized.class);
+		//Envoi du ID au serveur.
 		EventBusConnector bus = new EventBusConnector(1, listenedEvents, ip, 12045);
 		UIMainWindow window = new UIMainWindow(bus, "App Deux", "Avez", 2);
 		bus.addObserver(window);
--- trunk/labo2/src/events/EventAck.java
+++ trunk/labo2/src/events/EventAck.java
@@ -1,5 +1,18 @@
 package events;
-
+/******************************************************
+	Cours :           LOG730
+	Session :         Été 2010
+	Groupe :          01
+	Projet :          Laboratoire #2
+	Étudiants : Artom Lifshitz 
+				Chrystophe Chabert
+	Code(s) perm. : LIFA29108505
+					CHAC12098902
+	Date dernière modif: 2013-06-11
+	Date création :   2013-06-07
+******************************************************
+Nouvel évènement envoyé par le client au serveur.
+******************************************************/ 
 public class EventAck extends EventBase implements IEventAck {
 
 	private static final long serialVersionUID = -1156382431931313979L;
