0464130f5106935d601abc33c20b09c574384d4b
chrystopher.chabert <chrystopher.chabert@280fe1f3-3fe8-b438-d6e9-2b0f9534f95b>
2013-06-20 15:48:44 +0000

    Code de base fonctionnel pour Bank + BranchUpdater

--- labo3/src/ca/etsmtl/cours/log735/lab3/bank/Bank.java
+++ labo3/src/ca/etsmtl/cours/log735/lab3/bank/Bank.java
@@ -3,9 +3,15 @@ package ca.etsmtl.cours.log735.lab3.bank;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.UUID;
 
+/**
+ * Class Bank
+ * Classe en charge de recevoir les connexions de succursales et
+ * notifier celles-ci de l'ajout d'une nouvelle succursale sur le réseau.
+ * */
 public class Bank extends Thread{
 	
 	private HashMap<UUID, InetAddress> currentBranches;
@@ -24,8 +30,21 @@ public class Bank extends Thread{
 	
 	@Override
 	public void run(){
+		System.out.println("Bank is listening on port : " + PORT + " .. ");
 		while(true){
-			
+			if(bankSocket != null){
+				try {
+					Socket newBranch = bankSocket.accept();
+					System.out.println("New connexion received !");
+					new BranchUpdaterThread(newBranch, currentBranches).start();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else{
+				System.err.println("Bank socket is null, aborting.");
+				break;
+			}
 		}
 	}
 }
