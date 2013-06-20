943f727abbef5fa7d89485d62c3cf78bb44e4257
chrystopher.chabert <chrystopher.chabert@280fe1f3-3fe8-b438-d6e9-2b0f9534f95b>
2013-06-20 15:28:34 +0000

    Debut de code pour BankUpdater.

--- trunk/labo3/src/ca/etsmtl/cours/log735/lab3/bank/BranchUpdaterThread.java
+++ trunk/labo3/src/ca/etsmtl/cours/log735/lab3/bank/BranchUpdaterThread.java
@@ -2,12 +2,14 @@ package ca.etsmtl.cours.log735.lab3.bank;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.UUID;
 
 import ca.etsmtl.cours.log735.lab3.branch.Branch;
+import ca.etsmtl.cours.log735.message.HelloMessage;
 
 
 public class BranchUpdaterThread extends Thread{
@@ -15,6 +17,7 @@ public class BranchUpdaterThread extends Thread{
 	private Socket client;
 	private HashMap<UUID, InetAddress> currentBranches;
 	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
 	
 	public BranchUpdaterThread(Socket client, HashMap<UUID, InetAddress> currentBranches){
 		this.client = client;
@@ -24,15 +27,33 @@ public class BranchUpdaterThread extends Thread{
 	@Override
 	public void run(){
 		try {
+			InetAddress newBranchInetAddress = client.getInetAddress();
+			UUID newBranchUUIDToAdd;
+			
+			System.out.println("Server discovered new client on InetAddress : " + newBranchInetAddress);
+			
 			ois = new ObjectInputStream(client.getInputStream());
-			Branch newBranch;
-			while((newBranch = (Branch) ois.readObject()) != null){
+			Object newBranchMessage;
+			
+			newBranchMessage = ois.readObject();
+			//if the branch message is a hello message, we add it to the list of currentBranches,
+			//then we notify that client of all the current clients.
+			if(newBranchMessage instanceof HelloMessage){
+				//retrieve the uuid from the HelloMessage
+				newBranchUUIDToAdd = ((HelloMessage) newBranchMessage).getUuid();
+				//add to list of current branches
+				currentBranches.put(newBranchUUIDToAdd, newBranchInetAddress);
+				//notify the newly added client first.
+				oos = new ObjectOutputStream(client.getOutputStream());
+				//pass the whole list
+				oos.writeObject(currentBranches);
+				
 				
 			}
 		} catch (IOException e) {
-			System.err.println();
+			System.err.println("Server -> IOException occured : " + e.getCause());
 		} catch (ClassNotFoundException e) {
-			System.err.println("Server is unable to parse input stream object, aborting.");
+			System.err.println("Server is unable to parse input stream object,");
 		} finally{
 			try {
 				ois.close();
