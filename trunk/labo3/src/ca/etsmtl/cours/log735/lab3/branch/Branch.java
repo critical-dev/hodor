1189c34b4c18abfdff73a08970cf7651b932b8da
notartom@gmail.com <notartom@gmail.com@280fe1f3-3fe8-b438-d6e9-2b0f9534f95b>
2013-06-20 19:39:06 +0000

    Validation des parametres, connexion se fait au debut

--- trunk/labo3/src/ca/etsmtl/cours/log735/lab3/branch/Branch.java
+++ trunk/labo3/src/ca/etsmtl/cours/log735/lab3/branch/Branch.java
@@ -5,27 +5,25 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.Observable;
 import java.util.UUID;
 
 import ca.etsmtl.cours.log735.lab3.bank.Bank;
 import ca.etsmtl.cours.log735.message.HelloMessage;
 
-public class Branch {
+public class Branch extends Observable {
+	
+	public static final int PORT = 4343;
 	
 	private UUID branchId = UUID.randomUUID();
 	private BankListenerThread blt;
 	
-	public Branch(int initialMoney, InetAddress bankIp) {
-		try {
-			Socket sock = new Socket(bankIp, Bank.PORT);
-			ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
-			oos.writeObject(new HelloMessage(branchId, initialMoney));
-			blt = new BankListenerThread(this);
-			blt.start();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public Branch(int initialMoney, InetAddress bankIp) throws IOException {
+		Socket sock = new Socket(bankIp, Bank.PORT);
+		ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
+		oos.writeObject(new HelloMessage(branchId, initialMoney));
+		blt = new BankListenerThread(this);
+		blt.start();
 	}
 
 	public void refreshBranchList(HashMap<UUID, InetAddress> branchList) {
--- trunk/labo3/src/ca/etsmtl/cours/log735/lab3/branch/BranchGUI.java
+++ trunk/labo3/src/ca/etsmtl/cours/log735/lab3/branch/BranchGUI.java
@@ -1,16 +1,19 @@
 package ca.etsmtl.cours.log735.lab3.branch;
 
 import java.awt.BorderLayout;
-import java.awt.Dialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -18,49 +21,32 @@ import javax.swing.JToolBar;
 
 import net.miginfocom.swing.MigLayout;
 
-public class BranchGUI extends JFrame {
+public class BranchGUI extends JFrame implements Observer, ActionListener {
 
 	private static final long serialVersionUID = -3229493735001667568L;
 	
+	private static final String COMMAND_START = "START";
+	private static final String COMMAND_CAPTURE = "CAPTURE";
+	
 	private Branch branch;
 	
-	JTextArea captureLog;
+	JTextArea operationsLog;
 	JTextField bankIpField;
 	JTextField initialMoneyField;
-	
-	private class StartCaptureListener implements ActionListener {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			branch.captureState();
-		}
-	}
-	
-	private class StartBranchListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent ev) {
-			try {
-				InetAddress bankIp = InetAddress.getByName(bankIpField.getText());
-				int initialMoney = Integer.parseInt(initialMoneyField.getText());
-				branch = new Branch(initialMoney, bankIp);
-			} catch (UnknownHostException ex) {
-				// TODO Auto-generated catch block
-				ex.printStackTrace();
-			}
-		}
-	}
+	JDialog startDialog;
 
 	public BranchGUI(String appName) {
 		super(appName);
 		JPanel panel = new JPanel(new BorderLayout());
 		JToolBar toolbar = new JToolBar();
+		toolbar.setFloatable(false);
 		JButton stateCapture = new JButton("Capture d'état");
-		stateCapture.addActionListener(new StartCaptureListener());
+		stateCapture.setActionCommand(COMMAND_CAPTURE);
+		stateCapture.addActionListener(this);
 		toolbar.add(stateCapture);
 		panel.add(toolbar, BorderLayout.NORTH);
-		captureLog = new JTextArea();
-		panel.add(captureLog, BorderLayout.CENTER);
+		operationsLog = new JTextArea();
+		panel.add(operationsLog, BorderLayout.CENTER);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setContentPane(panel);
 		this.pack();
@@ -69,7 +55,7 @@ public class BranchGUI extends JFrame {
 	}
 	
 	private void init() {
-		JDialog startDialog = new JDialog(this, "Paramètres de démarrage", true);
+		startDialog = new JDialog(this, "Paramètres de démarrage", true);
 		JPanel panel = new JPanel(new MigLayout());
 		panel.add(new JLabel("Adresse IP de la banque"));
 		bankIpField = new JTextField(30);
@@ -78,7 +64,8 @@ public class BranchGUI extends JFrame {
 		initialMoneyField = new JTextField(30);
 		panel.add(initialMoneyField, "wrap");
 		JButton startButton = new JButton("Démarrer");
-		startButton.addActionListener(new StartBranchListener());
+		startButton.setActionCommand(COMMAND_START);
+		startButton.addActionListener(this);
 		panel.add(startButton);
 		startDialog.setContentPane(panel);
 		startDialog.pack();
@@ -87,5 +74,33 @@ public class BranchGUI extends JFrame {
 
 	public static void main(String[] args) {
 		new BranchGUI("Succursale");
+	}
+
+	@Override
+	public void update(Observable o, Object text) {
+		operationsLog.append((String) text);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent action) {
+		String command = action.getActionCommand();
+		if (command.equals(COMMAND_CAPTURE)) {
+			branch.captureState();
+		}
+		if (command.equals(COMMAND_START)) {
+			try {
+				InetAddress bankIp = InetAddress.getByName(bankIpField.getText());
+				int initialMoney = Integer.parseInt(initialMoneyField.getText());
+				branch = new Branch(initialMoney, bankIp);
+				branch.addObserver(this);
+				startDialog.setVisible(false);
+			} catch (UnknownHostException ex) {
+				JOptionPane.showMessageDialog(this, "Adresse IP incorrecte", "Erreur", JOptionPane.ERROR_MESSAGE);
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(this, "Montant initial incorrect", "Erreur", JOptionPane.ERROR_MESSAGE);
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(this, "Impossible de se connecter à la banque", "Erreur", JOptionPane.ERROR_MESSAGE);
+			}
+		}
 	}
 }
