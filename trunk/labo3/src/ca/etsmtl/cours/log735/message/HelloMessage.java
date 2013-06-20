96d55df023fd8838aa7df779e9448b95c3de6abd
notartom@gmail.com <notartom@gmail.com@280fe1f3-3fe8-b438-d6e9-2b0f9534f95b>
2013-06-20 15:21:07 +0000

    HelloMessage added

---
+++ trunk/labo3/src/ca/etsmtl/cours/log735/message/HelloMessage.java
@@ -0,0 +1,16 @@
+package ca.etsmtl.cours.log735.message;
+
+import java.util.UUID;
+
+public class HelloMessage {
+
+	private UUID uuid;
+	private int initialMoney;
+	
+	public UUID getUuid() {
+		return uuid;
+	}
+	public int getInitialMoney() {
+		return initialMoney;
+	}
+}
