708decdbe7768570859d7140af510add42832682
chrystopher.chabert@gmail.com <chrystopher.chabert@gmail.com@280fe1f3-3fe8-b438-d6e9-2b0f9534f95b>
2013-06-24 20:48:06 +0000

    Bank added code to display total known money amount.

--- trunk/labo3/src/ca/etsmtl/cours/log735/lab3/bank/BankGUI.java
+++ trunk/labo3/src/ca/etsmtl/cours/log735/lab3/bank/BankGUI.java
@@ -2,6 +2,7 @@ package ca.etsmtl.cours.log735.lab3.bank;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
@@ -14,9 +15,11 @@ import java.util.UUID;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class BankGUI implements Observer, MouseListener{
 	
@@ -30,6 +33,8 @@ public class BankGUI implements Observer, MouseListener{
 	private String bankBranchesList;
 	private static final String DEFAULT_HEADER = "Succursales connues :\n";
 	private JScrollPane bankListScrollPane;
+	private JLabel totalMoneyLabel;
+	private JTextField totalMoneyTextField;
 	
 	private Bank bank;
 	
@@ -50,6 +55,10 @@ public class BankGUI implements Observer, MouseListener{
 		bankFrame = new JFrame(bankFrameTitle);
 		bankPanel = new JPanel();
 		
+		totalMoneyLabel = new JLabel("Argent total dans le système :");
+		totalMoneyTextField = new JTextField();
+		totalMoneyTextField.setEditable(false);
+		
 		bankListenerBtn = new JButton("Demarrer");
 		bankListenerBtn.addMouseListener(this);
 		
@@ -62,6 +71,13 @@ public class BankGUI implements Observer, MouseListener{
 		bankPanel.setLayout(mgr);
 		bankPanel.add(bankListenerBtn,BorderLayout.WEST);
 		bankPanel.add(bankListScrollPane,BorderLayout.CENTER);
+		bankPanel.add(totalMoneyLabel, BorderLayout.SOUTH);
+		
+		JPanel southPanel = new JPanel();
+		southPanel.setLayout(new GridLayout(0, 1));
+		southPanel.add(totalMoneyLabel);
+		southPanel.add(totalMoneyTextField);
+		bankPanel.add(southPanel, BorderLayout.SOUTH);
 		
 		bankFrame.getContentPane().add(bankPanel);
 		bankFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -82,13 +98,24 @@ public class BankGUI implements Observer, MouseListener{
 	public void update(Observable arg0, Object arg1) {
 		if(arg0 instanceof Bank){
 			System.out.println("Updating frame branches list..");
+			//small delay introduced to let the listener thread work..
+			try {
+				Thread.sleep(5000);//to let updater thread set the new money amount
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
 			HashMap<UUID, InetAddress> branches = ((Bank) arg0).getCurrentBranches();
 			int i = 0;
+			bankBranchesList = DEFAULT_HEADER;
 			for(UUID id : branches.keySet()){
 				i++;
 				bankBranchesList += "Succursale ["+i+"]: "+id+"@"+branches.get(id)+"\n";
 			}
 			bankBranchesListArea.setText(bankBranchesList);
+			
+			//Update total known amount of money in the system
+			totalMoneyTextField.setText(Bank.BANK_TOTAL_MONEY_IN_THE_SYSTEM + "");
 		}
 	}
 
