7c3b0a49c8604a46d968f04b240f8e34bd9e0de5
notartom@gmail.com <notartom@gmail.com@280fe1f3-3fe8-b438-d6e9-2b0f9534f95b>
2013-07-01 19:15:52 +0000

    Branches have the code necessary to connect to one another, and export two ports: one for incoming bank connections, one for incoming branch connections. Code not yet tested.

--- trunk/labo3/src/ca/etsmtl/cours/log735/lab3/branch/BankListenerThread.java
+++ trunk/labo3/src/ca/etsmtl/cours/log735/lab3/branch/BankListenerThread.java
@@ -8,40 +8,39 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.UUID;
 
-import ca.etsmtl.cours.log735.lab3.bank.Bank;
-
 public class BankListenerThread extends Thread {
 	
 	private Branch branch;
-	private ServerSocket sock;
 
 	public BankListenerThread(Branch branch) {
-		try {
-			this.branch = branch;
-			sock = new ServerSocket(Bank.PORT);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.branch = branch;
 	}
 	
 	@Override
 	public void run() {
-		while(true) {
-			try {
+		try {
+			ServerSocket sock = new ServerSocket(Branch.BANK_PORT);
+			System.out.println("Listening for connection from bank...");
+			while (true) {
 				Socket conn = sock.accept();
+				System.out.println("Bank connected.");
 				ObjectInputStream ois = new ObjectInputStream(conn.getInputStream());
 				Object input = ois.readObject();
+				System.out.println("Received message from bank.");
 				if (input instanceof HashMap) {
 					branch.refreshBranchList((HashMap<UUID, InetAddress>) input);
+					System.out.println("Processed branch list update.");
 				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				ois.close();
+				conn.close();
+				System.out.println("Connection closed.");
 			}
+		} catch (IOException ioe) {
+			// TODO Auto-generated catch block
+			ioe.printStackTrace();
+		} catch (ClassNotFoundException cnfe) {
+			// TODO Auto-generated catch block
+			cnfe.printStackTrace();
 		}
 	}
 }
--- trunk/labo3/src/ca/etsmtl/cours/log735/lab3/branch/Branch.java
+++ trunk/labo3/src/ca/etsmtl/cours/log735/lab3/branch/Branch.java
@@ -1,10 +1,13 @@
 package ca.etsmtl.cours.log735.lab3.branch;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Observable;
 import java.util.UUID;
 
@@ -13,26 +16,57 @@ import ca.etsmtl.cours.log735.message.HelloMessage;
 
 public class Branch extends Observable {
 	
-	public static final int PORT = 4343;
+	public static final int BANK_PORT = 4343;
+	public static final int BRANCHES_PORT = 4444;
 	
-	private UUID branchId = UUID.randomUUID();
-	private BankListenerThread blt;
+	private List<UUID> peerIds = new LinkedList<UUID>();
+	private UUID myId = UUID.randomUUID();
+	
+	private List<ObjectInputStream> incomingChannels = new LinkedList<ObjectInputStream>();
+	private List<ObjectOutputStream> outgoingChannels = new LinkedList<ObjectOutputStream>();
 	
 	public Branch(int initialMoney, InetAddress bankIp) throws IOException {
+		new BankListenerThread(this).start();
+		new BranchesListenerThread(this).start();
 		Socket sock = new Socket(bankIp, Bank.PORT);
+		System.out.println("Connected to bank.");
 		ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
-		oos.writeObject(new HelloMessage(branchId, initialMoney));
-		blt = new BankListenerThread(this);
-		blt.start();
+		oos.writeObject(new HelloMessage(myId, initialMoney));
+		System.out.println("Hello sent to bank.");
+		oos.close();
+		sock.close();
 	}
 
 	public void refreshBranchList(HashMap<UUID, InetAddress> branchList) {
-		// TODO Auto-generated method stub
-		
+		for (UUID id: branchList.keySet()) {
+			if (!this.peerIds.contains(id) && id.compareTo(myId) != 0) {
+				connect(branchList.get(id));
+				this.peerIds.add(id);
+				System.out.println("Added " + id + " to peer list and connected.");
+			}
+		}
 	}
 
 	public void captureState() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public void addIncomingChannel(ObjectInputStream ois) {
+		incomingChannels.add(ois);
+	}
+	
+	private void addOutgoingChannel(ObjectOutputStream oos) {
+		outgoingChannels.add(oos);
+	}
+	
+	private void connect(InetAddress peer) {
+		try {
+			Socket sock = new Socket(peer, Branch.BRANCHES_PORT);
+			addOutgoingChannel(new ObjectOutputStream(sock.getOutputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
