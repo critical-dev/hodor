9e8243ae68ed211800d97a3b8e209354570fe7ed
chrystopher.chabert <chrystopher.chabert@gmail.com>
2013-07-03 05:26:01 +0000

    FIX WORKED, keeping code as it is, applied while loop to global state to test. Still keeping windows 7 enforcement to test with one VM sending to another for now.

--- labo3/src/ca/etsmtl/cours/log735/lab3/branch/SendCaptureThread.java
+++ labo3/src/ca/etsmtl/cours/log735/lab3/branch/SendCaptureThread.java
@@ -16,37 +16,41 @@ public class SendCaptureThread extends Thread {
 	private final static int DELAY_MAX = 30;
 	
 	private Branch branch;
-	private List<UUID> peerIDs = new ArrayList<UUID>();
 	
 	public SendCaptureThread(Branch branch) {
 		this.branch = branch;
-		peerIDs.addAll(branch.getOutgoingChannelsByUUID().keySet());
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//initial sleep time, just to make sure we have enough clients..
 	}
 
 	@Override
 	public void run() {
-		//while (true) {
-		System.out.println();
-		if(System.getProperty("os.name").toLowerCase().contains("windows 7")){
-			int delay = (int) (DELAY_MIN + (DELAY_MAX - DELAY_MIN) * Math.random());
-			try {
-				sleep(delay * 1000);
-				UUID id = branch.getOutgoingChannelsByUUID().keySet().iterator().next();
-				ObjectOutputStream oos = branch.getOutgoingChannelsByUUID().get(id);
-				System.out.println("I AM " + branch.getMyId());
-				System.out.println("Sending START capture message request to id : " + id);
-				oos.writeObject(new StateSyncStartMessage(branch.getMyId()));//request a state capture
-				sleep(6000); //sleep again
-				System.out.println("Sending STOP capture message request to id : " + id);
-				oos.writeObject(new StateSyncStopMessage(branch.getMyId()));//request the previous state capture's response
-			} catch (InterruptedException e) {
-				System.err.println(">> Error occured in sendCaptureThread !");
-				e.printStackTrace();
-			} catch (IOException e) {
-				System.err.println(">> Error occured in sendCaptureThread !");
-				e.printStackTrace();
+		while (true) {
+			System.out.println();
+			if(System.getProperty("os.name").toLowerCase().contains("windows 7")){
+				int delay = (int) (DELAY_MIN + (DELAY_MAX - DELAY_MIN) * Math.random());
+				try {
+					sleep(delay * 1000);
+					UUID id = branch.getOutgoingChannelsByUUID().keySet().iterator().next();
+					ObjectOutputStream oos = branch.getOutgoingChannelsByUUID().get(id);
+					System.out.println("I AM " + branch.getMyId());
+					System.out.println("Sending START capture message request to id : " + id);
+					oos.writeObject(new StateSyncStartMessage(branch.getMyId()));//request a state capture
+					sleep(6000); //sleep again
+					System.out.println("Sending STOP capture message request to id : " + id);
+					oos.writeObject(new StateSyncStopMessage(branch.getMyId()));//request the previous state capture's response
+				} catch (InterruptedException e) {
+					System.err.println(">> Error occured in sendCaptureThread !");
+					e.printStackTrace();
+				} catch (IOException e) {
+					System.err.println(">> Error occured in sendCaptureThread !");
+					e.printStackTrace();
+				}
 			}
-		//}
 		}
 	}
 }
