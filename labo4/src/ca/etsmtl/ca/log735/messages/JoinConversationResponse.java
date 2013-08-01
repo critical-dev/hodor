469e72e0c18356d55e17f63f7977f4cfa8f2d556
chrystopher.chabert <chrystopher.chabert@gmail.com>
2013-08-01 16:07:43 +0000

    Merge join group/room response en join conversation response.

--- labo4/src/ca/etsmtl/ca/log735/messages/JoinConversationResponse.java
+++ labo4/src/ca/etsmtl/ca/log735/messages/JoinConversationResponse.java
@@ -1,7 +1,7 @@
 package ca.etsmtl.ca.log735.messages;
 
 import ca.etsmtl.log735.client.Client;
-import ca.etsmtl.log735.model.Group;
+import ca.etsmtl.log735.model.Conversation;
 /******************************************************
 Cours : LOG735
 Session : Été 2013
@@ -12,16 +12,16 @@ Code(s) perm. : LIFA29108505, CHAC12098902
 Date création : 01/07/2013
 ******************************************************/
 /**
- * With this message, the server tells the client that the join group request succeeded or failed.
+ * With this message, the server tells the client that the join group/room request succeeded or failed.
  * @author chrys, artom
  *
  */
-public class JoinGroupResponse extends ClientMessage {
+public class JoinConversationResponse extends ClientMessage {
 
-	private Group group;
+	private Conversation convo;
 	
-	public JoinGroupResponse(Group group){
-		this.group = group;
+	public JoinConversationResponse(Conversation convo){
+		this.convo = convo;
 	}
 	
 	@Override
